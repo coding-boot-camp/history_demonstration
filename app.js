635a479e09b6ed51f8388d79c967807377976344
Peleke <peleke@trilogyed.com>
2017-07-06 15:00:10 -0400

    :sparkle: Display search results on home page.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
   $("#search-button").on("click", function (event) {
-    console.log("Click listener works!");
+    var itemCount = 10;
+    var resultsList = $("#results-list")
+
+    $("#no-results").remove();
+
+    for (var i = 0; i < itemCount; i += 1) {
+      var result = $("<li>")
+      result.text("Result #" + i);
+      resultsList.append(result);
+    }
   })
 })
